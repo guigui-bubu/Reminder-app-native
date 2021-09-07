8d45fa17e04e65d18f1dab7d7ffa25a8717969dc
guigui-bubu <guillaumebuaud@yahoo.fr>
2021-09-07 19:15:51 +0200

    ajout permission

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 // Librairies
-import React from "react";
+import React, { useEffect } from "react";
+import * as Notifications from "expo-notifications";
 
 // Redux
 import { createStore, applyMiddleware } from "redux"; // applyMiddleware => pour appliquer un middleware
@@ -13,6 +14,19 @@ import AppNavigator from "./navigation/AppNavigators";
 const store = createStore(appReducer, applyMiddleware(thunk)); // applyMiddleware => pour ajouter notre Middleware
 
 export default function App() {
+  // Demander la permission pour envoyer des Notifs
+  useEffect(() => {
+    const getPermissions = async () => {
+      const { status } = await Notifications.getPermissionsAsync();
+      let finalStatus = status;
+      if (status !== "granted") {
+        const { requestStatus } = await Notifications.requestPermissionsAsync();
+        finalStatus = requestStatus;
+      }
+    };
+    getPermissions();
+  }, []);
+
   return (
     <Provider store={store}>
       <AppNavigator />
